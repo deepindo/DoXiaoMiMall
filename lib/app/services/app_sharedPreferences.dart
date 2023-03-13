3e683fa65940cd694c02b0e99e520e6679f9ac60
deepindo <732872042@qq.com>
2023-03-13 08:13:34 +0800

    添加shared_preferences插件，并对其封装

---
+++ lib/app/services/app_sharedPreferences.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class DoSharedPreferences {
+  ///设置
+  static setData(String key, dynamic value) async {
+    var sp = await SharedPreferences.getInstance();
+    sp.setString(key, jsonEncode(value));
+  }
+
+  ///获取
+  static getData(String key) async {
+    try {
+      var sp = await SharedPreferences.getInstance();
+      String? s = sp.getString(key);
+
+      if (s != null) {
+        return json.decode(s);
+      } else {
+        return null;
+      }
+    } catch (e) {
+      // print(e);
+      return e.toString();
+    }
+  }
+
+  ///移除
+  static removeData(String key) async {
+    var sp = await SharedPreferences.getInstance();
+    sp.remove(key);
+  }
+
+  ///清空
+  static clearData(String key) async {
+    var sp = await SharedPreferences.getInstance();
+    sp.clear();
+  }
+}
